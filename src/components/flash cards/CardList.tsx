374813d624e5d37671ddb10e1b6b14b435745062
David Orson <onlyorson@outlook.com>
2020-04-22 14:54:08 +0100

    Card Summary Module

--- src/components/flash cards/CardList.tsx
+++ src/components/flash cards/CardList.tsx
@@ -1,23 +1,12 @@
 import React from "react";
+import CardSummary from "./CardSummary";
 
 const CardList = () => {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h4 className="card-title">Card Title</h4>
-          <p>By David Orson</p>
-          <p className="caption">22nd April, 2pm</p>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="card-content">
-          <h4 className="card-title">Card Title</h4>
-          <p>By David Orson</p>
-          <p className="caption">22nd April, 2pm</p>
-        </div>
-      </div>
+      <CardSummary />
+      <CardSummary />
+      <CardSummary />
     </div>
   );
 };
--- src/components/flash cards/CardSummary.tsx
+++ src/components/flash cards/CardSummary.tsx
@@ -1,1 +1,15 @@
-export {};
+import React from "react";
+
+const CardSummary = () => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h4 className="card-title">Card Title</h4>
+        <p>By David Orson</p>
+        <p className="caption">22nd April, 2pm</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardSummary;
