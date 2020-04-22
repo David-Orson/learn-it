22f78250f2ab6a80d7b465117b20dc1525931c45
David Orson <onlyorson@outlook.com>
2020-04-22 18:40:02 +0100

    Firebase Config

---
+++ src/config/fbConfig.tsx
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Ftg3dbdsMrwxfpvBoNMgSu5-1HzB3-g",
+  authDomain: "learn-it-31224.firebaseapp.com",
+  databaseURL: "https://learn-it-31224.firebaseio.com",
+  projectId: "learn-it-31224",
+  storageBucket: "learn-it-31224.appspot.com",
+  messagingSenderId: "607175907969",
+  appId: "1:607175907969:web:a3e5571ba2be3787674b80",
+  measurementId: "G-4MVKYV5C0M",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
